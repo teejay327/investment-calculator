a5e6da9d7a24e4aef935a4bb90ca59bbb8254d71
teejay327 <mrenergy@hotmail.com>
2023-06-16 00:28:26 +1000

    Added handlers for form submission userInput.js

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -2,16 +2,34 @@ import React from 'react';
 import './UserInput.css';
 
 const UserInput = () => {
+  const submitHandler = event => {
+    event.preventDefault();
+    const financialData = {
+      //currentSavings: current-savings
+    }
+    console.log('Submit')
+  };
+
+  const resetHandler = event => {
+    console.log('Reset');
+  }
+
+  const inputChangeHandler = (input, value) => {
+    console.log(input, value);
+  }
+
   return (
-    <form className="form">
+    <form onSubmit={ submitHandler } className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input onChange={(event) => inputChangeHandler('currentSavings', event.target.value)}
+           type="number" id="current-savings" />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input onChange={(event) => inputChangeHandler('yearly-contribution', event.target.value)} 
+          type="number" id="yearly-contribution" />
         </p>
       </div>
       <div className="input-group">
@@ -19,15 +37,17 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input onChange={(event) => inputChangeHandler('expected-return', event.target.value)} 
+          type="number" id="expected-return" />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input onChange={(event) => inputChangeHandler('duration', event.target.value)} 
+          type="number" id="duration" />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button onClick={ resetHandler } type="reset" className="buttonAlt">
           Reset
         </button>
         <button type="submit" className="button">
